2559cf333bc8ac92d0e1f249f0490627dea34d67
Tiago de Lima <vercosa@proton.me>
2025-08-25 22:18:32 -0300

    fix conceal

--- lua/user/plugins/vim-tex.lua
+++ lua/user/plugins/vim-tex.lua
@@ -7,8 +7,16 @@ return {
         vim.g.tex_flavor = 'latex'
         vim.g.vimtex_view_method = "sioyek"
         vim.g.vimtex_quickfix_mode = 0
-        vim.g.conceallevel = 1
         vim.g.tex_conceal = 'abdmg'
+
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = 'tex',
+            callback = function()
+                vim.opt_local.conceallevel = 2
+                vim.opt_local.concealcursor = 'n'
+            end,
+            desc = 'VimTeX conceal settings'
+        })
 
         vim.g.vimtex_compiler_latexmk = {
             executable = 'latexmk',
