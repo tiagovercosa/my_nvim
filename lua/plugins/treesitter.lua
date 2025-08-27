8e7c4ab5ccfda73d9fbcad9737178bc878bd0ebc
Tiago de Lima <vercosa@proton.me>
2025-08-27 20:01:49 -0300

    adjust tab and indentation

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,10 +6,12 @@ return {
         require('nvim-treesitter.configs').setup {
             ensure_installed = { 'c', 'lua', 'python', 'cpp', 'fortran', 'latex' },
             auto_install = true,
+            sync_install = true,
             highlight = {
               enable = true,
               disable = { "latex" },
             },
+            indent = { enable = true },
         }
     end
 }
