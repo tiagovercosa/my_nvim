a606d689a5717735a40de090db91f96bba816377
Tiago de Lima <vercosa@proton.me>
2025-09-01 00:55:44 -0300

    changed pdf reader for skim

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -3,11 +3,22 @@
 return {
   'lervag/vimtex',
   lazy = false,
-  init = function ()
+  init = function()
     vim.g.tex_flavor = 'latex'
-    vim.g.vimtex_view_method = "sioyek"
+    vim.g.vimtex_view_method = "skim"
+    vim.g.vimtex_view_skim_activate = 1
+    vim.g.vimtex_view_skim_sync = 1
+    vim.g.vimtex_view_skim_reading_bar = 1
     vim.g.vimtex_quickfix_mode = 0
     vim.g.tex_conceal = 'abdmg'
+
+    -- Configurações de completude
+    vim.g.vimtex_complete_enabled = 1
+    vim.g.vimtex_complete_close_braces = 1
+
+    -- Configurações de fold
+    vim.g.vimtex_fold_enabled = 1
+    vim.g.vimtex_fold_manual = 1
 
     vim.api.nvim_create_autocmd('FileType', {
       pattern = 'tex',
@@ -27,6 +38,5 @@ return {
         '-synctex=1',
       },
     }
-
   end
 }
