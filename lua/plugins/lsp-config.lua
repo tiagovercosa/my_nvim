cea1d66cf1e8b14f6dbc4c7c820dc6baa67fb823
Tiago de Lima <vercosa@proton.me>
2025-09-04 12:59:48 -0300

    removed dependencies plugin

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -46,7 +46,6 @@ return {
     "antosha417/nvim-lsp-file-operations",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      "nvim-neo-tree/neo-tree.nvim",
       config = function()
         require("lsp-file-operations").setup()
       end,
